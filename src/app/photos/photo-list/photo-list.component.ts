0c1320f3b07bbfee4e6ba8d137c21e1f36c23731
Estevao Vasques <estevaovasques@hotmail.com>
2020-09-21 13:13:37 -0300

    aula 6

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,29 +1,51 @@
-import { Component, OnInit } from '@angular/core';
-import { PhotoService } from '../photo/photo.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
 import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'ap-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
-  photos: Photo[] = [];
-  filter: string = '';
+    photos: Photo[] = [];
+    filter: string = '';
+    debounce: Subject<string> = new Subject<string>();
+    hasMore: boolean = true;
+    currentPage: number = 1;
+    userName: string = '';
 
-  constructor(
-    private photoService: PhotoService,
-    private activatedRoute: ActivatedRoute
-  ) { }
+    constructor(
+        private activatedRoute: ActivatedRoute,
+        private photoService: PhotoService
+    ) { }
 
-  ngOnInit(): void {
-    const userName = this.activatedRoute.snapshot.params.userName;
+    ngOnInit(): void {
+        this.userName = this.activatedRoute.snapshot.params.userName;
+        this.photos = this.activatedRoute.snapshot.data['photos'];
+        this.debounce
+            .pipe(debounceTime(1000))
+            .subscribe(filter => this.filter = filter);
+    }
 
-    this.photoService
-      .listFromUser(userName)
-      .subscribe(photos => this.photos = photos);
-  }
+    ngOnDestroy(): void {
+        this.debounce.unsubscribe();
+    }
+
+    load() {
+        this.photoService
+            .listFromUserPaginated(this.userName, ++this.currentPage)
+            .subscribe(photos => {
+                this.photos = this.photos.concat(photos);
+
+                if (!photos.length) this.hasMore = false;
+
+            })
+    }
 
 }
