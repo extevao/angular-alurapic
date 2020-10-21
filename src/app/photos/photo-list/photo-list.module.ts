fb3ed18ecc6b3eae71718c9c3e925bdac482deee
Estevao Vasques <estevaovasques@hotmail.com>
2020-10-21 20:02:37 -0300

    Ultimo commit modulo 1

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from "@angular/core";
 
 
 import { CardModule } from "src/app/shared/components/card/card.module";
+import { DarkenOnHoverModule } from "src/app/shared/directives/darken-on-hover/darken-on-hover.module";
 import { PhotoModule } from "../photo/photo.module";
 import { FilterByDescription } from "./filter-by-description.pipe";
 import { LoadButtonComponent } from "./load-button/load-button.component";
@@ -21,7 +22,8 @@ import { SearchComponent } from "./search/search.component";
     imports: [
       CommonModule,
       PhotoModule,
-      CardModule
+      CardModule,
+      DarkenOnHoverModule
     ]
 })
 export class PhotoListModule {
