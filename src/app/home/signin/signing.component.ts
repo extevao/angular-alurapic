a612ac8ee14f6fade406f1d69b082abe8401c989
Estevao Vasques <estevaovasques@hotmail.com>
2020-10-24 20:36:51 -0300

    Realizando login e redirecionamento

--- src/app/home/signin/signing.component.ts
+++ src/app/home/signin/signing.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+
+import { AuthService } from "src/app/core/auth/auth.service";
 
 @Component({
   templateUrl: './signing.component.html'
@@ -9,13 +12,34 @@ export class SignInComponent implements OnInit {
   loginForm: FormGroup;
 
   constructor(
-    private formBuilder: FormBuilder
-  ){ }
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       userName: ['', Validators.required],
       password: ['', Validators.required]
     });
+  }
+
+  login() {
+    console.log('vai se autenticar')
+
+    const userName = this.loginForm.get('userName').value
+    const password = this.loginForm.get('password').value
+
+    this.authService
+      .authenticate(userName, password)
+      .subscribe(
+        () => {
+          console.log('AUTENTICADO')
+          this.router.navigate([ 'user', userName ])
+        }, err => {
+          console.log(err)
+
+          this.loginForm.reset()
+        })
   }
 }
